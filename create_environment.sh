83e797056f60cb82d96f85050a7797b03e31e014
armeleger <a.kayisire@alustudent.com>
2025-02-20 08:10:33 -0800

    Push just incase

--- create_environment.sh
+++ create_environment.sh
@@ -15,33 +15,29 @@ mkdir -p "$home_dir/modules"
 mkdir -p "$home_dir/assets"
 mkdir -p "$home_dir/config"
 
-#creating startup file
 
-touch "$home_dir/startup.sh"
+#config.env file
+echo "# This is the config file
+export ASSIGNMENT="Shell Navigation"
+export DAYS_REMAINING=2" >"$home_dir/config/config.env"
 
-#creating reminder.sh file
 
+#submissions.txt file
+echo "student, assignment, submission status
+Chinemerem, Shell Navigation, not submitted
+Chiagoziem, Git, submitted
+Divine, Shell Navigation, not submitted
+Anissa, Shell Basics, submitted
+Armel, Shell Navigation, not submitted
+Arnold, Git, Submitted
+Tito, Shell Navigation, not submitted
+Seth, Shell Navigation, submitted
+Elvis, Git, not submitted
+" >"$home_dir/assets/submissions.txt"
+
+
+#function.sh file
 echo "#!/bin/bash
-
-# Source environment variables and helper functions
-source ./config/config.env
-source ./modules/functions.sh
-
-# Path to the submissions file
-submissions_file="./assets/submissions.txt"
-
-# Print remaining time and run the reminder function
-echo "Assignment: $ASSIGNMENT"
-echo "Days remaining to submit: $DAYS_REMAINING days"
-echo "--------------------------------------------"
-
-check_submissions $submissions_file
-" > "$home_dir/app/reminder.sh"
-
-#creating function.sh file
-
-echo "#!/bin/bash
-
 # Function to read submissions file and output students who have not submitted
 function check_submissions {
     local submissions_file=$1
@@ -59,28 +55,34 @@ function check_submissions {
             echo "Reminder: $student has not submitted the $ASSIGNMENT assignment!"
         fi
     done < <(tail -n +2 "$submissions_file") # Skip the header
-}
-" >"$home_dir/modules/submissions.txt"
+}">"$home_dir/modules/functions.s"
 
-#creating submissions.txt file
 
-echo " student, assignment, submission status
-Chinemerem, Shell Navigation, not submitted
-Chiagoziem, Git, submitted
-Divine, Shell Navigation, not submitted
-Anissa, Shell Basics, submitted
-Armel, Shell Navigation, not submitted
-Arnold, Git, Submitted
-Tito, Shell Navigation, not submitted
-Seth, Shell Navigation, submitted
-Elvis, Git, not submitted
-" > "$home_dir/assets/submission.txt"
+#reminder.sh file
+echo "#!/bin/bash
+# Source environment variables and helper functions
+source ./config/config.env
+source ./modules/functions.sh
+# Path to the submissions file
+submissions_file="./assets/submissions.txt"
+#Print remaining time and run the reminder function
+echo "Assignment: $ASSIGNMENT"
+echo "Days remaining to submit: $DAYS_REMAINING days"
+echo "-------------------------------------------"
+check_submissions$submissions_file
+" >"$home_dir/app/reminder.sh"
 
-#creating config.env file
 
-echo " # This is the config file
-ASSIGNMENT="Shell Navigation"
-DAYS_REMAINING=2
-" > "$home_dir/config/config.env"
+#startup file
+echo "#!/bin/bash
+echo "Starting up the app"
+./app/reminder.sh" > "$home_dir/startup.sh"
 
+
+#User's Message
+echo "
+-----------------------------------------------
+Successfully Created ${username}'s Directory
+-----------------------------------------------
+"
 
